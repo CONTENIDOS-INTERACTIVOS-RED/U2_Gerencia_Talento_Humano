1c68c47dba4f3f3ccb9b7ef6d1def8f5c932e8a2
Francisco Lizcano <flizcanor@icloud.com>
2025-03-24 19:23:43 -0500

    se carga sintesis

--- src/views/curso/Sintesis.vue
+++ src/views/curso/Sintesis.vue
@@ -2,10 +2,10 @@
   .curso-main-container.creditos-vista
     BannerInterno(subTitulo="SÍNTESIS")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-      p(data-aos="fade-up").mb-5 La Unidad 3: Bioética y sostenibilidad en el desarrollo económico global, establece los principios esenciales para comprender cómo los valores éticos y bioéticos pueden orientar el desarrollo económico hacia la sostenibilidad. Esta unidad proporciona una perspectiva integral sobre la relación entre la economía, el medio ambiente y los derechos humanos, destacando la importancia de prácticas responsables en contextos globalizados. Este enfoque asegura que los estudiantes adquieran una comprensión sólida de los desafíos éticos actuales y las estrategias sostenibles, permitiéndoles aplicar estas herramientas de manera efectiva en su formación profesional y en la vida cotidiana.
+      p(data-aos="fade-up").mb-5 La presente unidad se centró en el proceso integral de gestión del talento humano como un elemento clave para la generación de valor y el éxito organizacional. A lo largo de esta unidad, se evaluaron y seleccionaron perfiles laborales adecuados, se diseñaron procesos de gestión del talento y se crearon planes de compensación equitativos.
 
       .row.justify-content-center
         .col-lg-12.mb-5
           figure.bg-color-sintesis.p-5.brounded
-            img(src='@/assets/curso/sintesis.svg', alt='', data-aos="zoom-in")
+            img(src='@/assets/curso/sintesis.png', alt='', data-aos="zoom-in")
 </template>
